7ccbfba7f8bd27aca0d21f746e5a2478f9370f63
kingdown88 <qc_f741aed4084e49c3b40354525e0bb7b2@git.code.tencent.com>
2018-11-12 13:46:24 +0800

    rent

--- flutter_movie/lib/house/net/net_util.dart
+++ flutter_movie/lib/house/net/net_util.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:device_info/device_info.dart';
 import 'package:dio/dio.dart';
 
 getDio() {
@@ -6,19 +10,33 @@ getDio() {
   return new Dio(options);
 }
 
-getOptions() {
+getOptions() async {
   var options = new Options();
   Map<String, dynamic> heads = new Map();
-  heads['ClientVer'] = "5.4.1";
+
+  var width = window.physicalSize.width;
+  var height = window.physicalSize.height;
+  heads['ScreenSize'] = width.toString() + "x" + height.toString();
+
+  DeviceInfoPlugin deviceInfo = new DeviceInfoPlugin();
+  if (Platform.isIOS) {
+    heads['Platform'] = "ios";
+    IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+    heads['os_model'] = iosInfo.model;
+    heads['OSVer'] = "ios" + iosInfo.systemVersion;
+  } else if (Platform.isAndroid) {
+    heads['Platform'] = "android";
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    heads['os_model'] = androidInfo.model;
+    heads['OSVer'] = "android" + androidInfo.version.release;
+  }
+
   heads['CityCode'] = "001009001";
-  heads['ScreenSize'] = "810x1440";
   heads['Session'] = "";
-  heads['Platform'] = "android";
   heads['Udid'] = "434159512668216";
-  heads['os_model'] = "Netease_MuMu";
   heads['md5'] = "90f8a9fe3f48619e8f829c33dd1395fd";
   heads['channel'] = "devoffline";
-  heads['OSVer'] = "android6.0.1";
+  heads['ClientVer'] = "5.4.1";
   options.headers = heads;
   return options;
 }
