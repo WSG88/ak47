b700c23e7d7e8191f69cb919da77778ba6a063cf
kingdown88 <qc_f741aed4084e49c3b40354525e0bb7b2@git.code.tencent.com>
2018-11-13 14:48:37 +0800

    gt7

---
+++ testng/src/main/java/gt/AppPush.java
@@ -0,0 +1,71 @@
+package gt;
+
+import com.gexin.rp.sdk.base.IPushResult;
+import com.gexin.rp.sdk.base.impl.AppMessage;
+import com.gexin.rp.sdk.http.IGtPush;
+import com.gexin.rp.sdk.template.LinkTemplate;
+import com.gexin.rp.sdk.template.TransmissionTemplate;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppPush {
+
+  //定义常量, appId、appKey、masterSecret 采用本文档 "第二步 获取访问凭证 "中获得的应用配置7
+  private static String appId = "MK3XtHhFmv97ItHERcrz5677";
+  private static String appKey = "QiD4lK2oVX6knIXhADvlT477";
+  private static String masterSecret = "hiA2QhYCGL8lTSWySzR2h777";
+  private static String url = "http://sdk.open.api.igexin.com/apiex.htm";
+
+  public static void main(String[] args) throws IOException {
+
+    IGtPush push = new IGtPush(url, appKey, masterSecret);
+
+    // 定义"点击链接打开通知模板"，并设置标题、内容、链接
+    getLinkTemplate();
+
+    List<String> appIds = new ArrayList<String>();
+    appIds.add(appId);
+
+    // 定义"AppMessage"类型消息对象，设置消息内容模板、发送的目标App列表、是否支持离线发送、以及离线消息有效期(单位毫秒)
+    AppMessage message = new AppMessage();
+    message.setData(transmissionTemplateDemo());
+    message.setAppIdList(appIds);
+    message.setOffline(true);
+    message.setOfflineExpireTime(1000 * 600);
+
+    IPushResult ret = push.pushMessageToApp(message);
+    System.out.println(ret.getResponse().toString());
+  }
+
+  private static LinkTemplate getLinkTemplate() {
+    LinkTemplate template = new LinkTemplate();
+    template.setAppId(appId);
+    template.setAppkey(appKey);
+    template.setTitle("请填写通知标题");
+    template.setText("请填写通知内容");
+    template.setUrl("http://getui.com");
+    return template;
+  }
+
+  public static TransmissionTemplate transmissionTemplateDemo() {
+    TransmissionTemplate template = new TransmissionTemplate();
+    template.setAppId(appId);
+    template.setAppkey(appKey);
+    // 透传消息设置，1为强制启动应用，客户端接收到消息后就会立即启动应用；2为等待应用启动
+    template.setTransmissionType(2);
+    template.setTransmissionContent("{\n"
+        + "\t\"task\": 1,\n"
+        + "\t\"push_id\": \"KI452ZVC90\",\n"
+        + "\t\"data\": {\n"
+        + "\t\t\"notify_id\": \"SXUI128XSAUG\",\n"
+        + "\t\t\"web_url\": \"https://www.baidu.com\",\n"
+        + "\t\t\"title\": \"111111\",\n"
+        + "\t\t\"content\": \"SXUI128XSAUG\"\n"
+        + "\t}\n"
+        + "}");
+    // 设置定时展示时间
+    // template.setDuration("2015-01-16 11:40:00", "2015-01-16 12:24:00");
+    return template;
+  }
+}
