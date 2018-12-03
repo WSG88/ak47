8847b9e77e08cc6b8cf5bf55dc506e3fdce700db
kingdown88 <qc_f741aed4084e49c3b40354525e0bb7b2@git.code.tencent.com>
2018-12-03 10:23:34 +0800

    test sql

--- src/main/java/util/TestSql.java
+++ src/main/java/util/TestSql.java
@@ -1,68 +1,101 @@
 package util;
 
+import com.alibaba.fastjson.JSONObject;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wsig on 2018-11-30.
  */
 class TestSql {
+  static Connection con = null;
+
+  static String driver, url, user, password, sql;
+
   public static void main(String[] args) {
-    Connection con;
-    String driver = "com.mysql.jdbc.Driver";
-    String url = "jdbc:mysql://localhost:3306/gaodu_statistics";
-    String user = "root";
-    String password = "root";
+    driver = "com.mysql.jdbc.Driver";
+    url = "jdbc:mysql://localhost:3306/gaodu_statistics";
+    user = "root";
+    password = "root";
     try {
       Class.forName(driver);
       con = DriverManager.getConnection(url, user, password);
       Statement statement = con.createStatement();
-      String sql = "select * from statistic limit 100";
-      //sql = "select * from statistic where ship_id='da8fc177-9cd2-18fd-9edf-dba07d7718b7'";
+      sql = "select * from statistic limit 3";
+      sql = "select * from statistic where ship_id='da8fc177-9cd2-18fd-9edf-dba07d7718b7'";
+      sql = "select count(*) as cn from statistic";
+
       ResultSet rs = statement.executeQuery(sql);
+      System.out.println(convertList(rs));
       while (rs.next()) {
 
-        String ship_id = "";
-        String type = "0";
-        String view_no = "0";
-        String call_no = "0";
-        String call_success_no = "0";
-        String reserve_no = "0";
-        String house_no = "0";
-        String refresh_no = "0";
-        String liked_no = "0";
-        String link_no = "0";
-        String im_no = "0";
-        String timestamp = "0";
-        String city_code = "";
-        String content = "";
-
-        System.out.println(rs.getString("id"));
-        System.out.println(rs.getString("timestamp"));
-
-        int timestampDate = getMonth(rs.getString("timestamp") + "000");
-        System.out.println("timestampDate=" + timestampDate);
       }
       rs.close();
-      con.close();
+      System.out.println("数据库数据成功获取！！");
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
-      System.out.println("数据库数据成功获取！！");
+      try {
+        if (con != null) {
+          con.close();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
     }
   }
 
-  public static int getMonth(String millis) {
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
-    Calendar clendar = Calendar.getInstance();
-    clendar.setTimeInMillis(Long.parseLong(millis));
-    Date date = clendar.getTime();
-    System.out.println(sdf.format(date));
-    return date.getMonth() + 1;
+  private static List convertList(ResultSet rs) throws SQLException {
+    List<JSONObject> list = new ArrayList<>();
+    ResultSetMetaData md = (ResultSetMetaData) rs.getMetaData();
+    int column = md.getColumnCount();
+    while (rs.next()) {
+      Map<String, Object> rowData = new HashMap<>();
+      for (int i = 1; i <= column; i++) {
+        rowData.put(md.getColumnName(i).toString(), rs.getObject(i));
+      }
+      JSONObject json = new JSONObject(rowData);
+      System.out.println("-------------------------------------------");
+      System.out.println(json);
+      list.add(json);
+    }
+    return list;
+  }
+
+  class Statistic {
+    String ship_id = "";
+    String type = "0";
+    String view_no = "0";
+    String call_no = "0";
+    String call_success_no = "0";
+    String reserve_no = "0";
+    String house_no = "0";
+    String refresh_no = "0";
+    String liked_no = "0";
+    String link_no = "0";
+    String im_no = "0";
+    String timestamp = "0";
+    String city_code = "";
+    String content = "";
+
+    int getMonth(String millis) {
+      SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+      Calendar calendar = Calendar.getInstance();
+      calendar.setTimeInMillis(Long.parseLong(millis));
+      Date date = calendar.getTime();
+      System.out.println(simpleDateFormat.format(date));
+      return date.getMonth() + 1;
+    }
   }
 }
