bd7dd1721c6942a680046892d0b06f7ddf390cfe
kingdown88 <qc_f741aed4084e49c3b40354525e0bb7b2@git.code.tencent.com>
2019-03-29 10:38:24 +0800

    pom

--- src/main/java/file_util/ImgReduce.java
+++ src/main/java/file_util/ImgReduce.java
@@ -1,88 +1,77 @@
 package file_util;
 
-import com.sun.image.codec.jpeg.JPEGCodec;
-import com.sun.image.codec.jpeg.JPEGImageEncoder;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import javax.imageio.ImageIO;
-
 /**
  * 图片压缩处理
  */
 public class ImgReduce {
 
-  public static void reduceImg(String s, float rate) {
-    reduceImg(s, 0, 0, rate);
-  }
-
-  public static void reduceImg(String s, int w, int h, float rate) {
-    try {
-      File file = new File(s);
-      if (!file.exists()) {
-        System.out.println("文件不存在");
-      }
-
-      if (rate > 0) {
-        int[] results = getImgWidthHeight(file);
-        if (results == null || results[0] == 0 || results[1] == 0) {
-          return;
-        } else {
-          w = (int) (results[0] * rate);
-          h = (int) (results[1] * rate);
-        }
-      }
-
-      Image image = ImageIO.read(file);
-      BufferedImage bufferedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
-      bufferedImage.getGraphics()
-          .drawImage(image.getScaledInstance(w, h, Image.SCALE_SMOOTH), 0, 0, null);
-
-      String filePath = file.getAbsolutePath();
-      filePath = filePath.replace(".jpg", "_new.jp");
-      System.out.println(filePath);
-
-      FileOutputStream fileOutputStream = new FileOutputStream(filePath);
-      JPEGImageEncoder jpegImageEncoder = JPEGCodec.createJPEGEncoder(fileOutputStream);
-      jpegImageEncoder.encode(bufferedImage);
-      fileOutputStream.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  public static int[] getImgWidthHeight(File file) {
-    InputStream inputStream = null;
-    BufferedImage bufferedImage;
-    int[] results = { 0, 0 };
-    try {
-      inputStream = new FileInputStream(file);
-      bufferedImage = ImageIO.read(inputStream);
-      results[0] = bufferedImage.getWidth(null);
-      results[1] = bufferedImage.getHeight(null);
-    } catch (Exception e) {
-      e.printStackTrace();
-    } finally {
-      try {
-        if (inputStream != null) {
-          inputStream.close();
-        }
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-    return results;
-  }
-
-  public static void main(String[] args) {
-    File file = new File("C:\\Users\\DELL\\Desktop\\my");
-    File[] fs = file.listFiles();
-    for (File f : fs) {
-      reduceImg(f.getAbsolutePath(), 0.3f);
-    }
-  }
+  //public static void reduceImg(String s, float rate) {
+  //  reduceImg(s, 0, 0, rate);
+  //}
+  //
+  //public static void reduceImg(String s, int w, int h, float rate) {
+  //  try {
+  //    File file = new File(s);
+  //    if (!file.exists()) {
+  //      System.out.println("文件不存在");
+  //    }
+  //
+  //    if (rate > 0) {
+  //      int[] results = getImgWidthHeight(file);
+  //      if (results == null || results[0] == 0 || results[1] == 0) {
+  //        return;
+  //      } else {
+  //        w = (int) (results[0] * rate);
+  //        h = (int) (results[1] * rate);
+  //      }
+  //    }
+  //
+  //    Image image = ImageIO.read(file);
+  //    BufferedImage bufferedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+  //    bufferedImage.getGraphics()
+  //        .drawImage(image.getScaledInstance(w, h, Image.SCALE_SMOOTH), 0, 0, null);
+  //
+  //    String filePath = file.getAbsolutePath();
+  //    filePath = filePath.replace(".jpg", "_new.jp");
+  //    System.out.println(filePath);
+  //
+  //    FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+  //    JPEGImageEncoder jpegImageEncoder = JPEGCodec.createJPEGEncoder(fileOutputStream);
+  //    jpegImageEncoder.encode(bufferedImage);
+  //    fileOutputStream.close();
+  //  } catch (Exception e) {
+  //    e.printStackTrace();
+  //  }
+  //}
+  //
+  //public static int[] getImgWidthHeight(File file) {
+  //  InputStream inputStream = null;
+  //  BufferedImage bufferedImage;
+  //  int[] results = { 0, 0 };
+  //  try {
+  //    inputStream = new FileInputStream(file);
+  //    bufferedImage = ImageIO.read(inputStream);
+  //    results[0] = bufferedImage.getWidth(null);
+  //    results[1] = bufferedImage.getHeight(null);
+  //  } catch (Exception e) {
+  //    e.printStackTrace();
+  //  } finally {
+  //    try {
+  //      if (inputStream != null) {
+  //        inputStream.close();
+  //      }
+  //    } catch (IOException e) {
+  //      e.printStackTrace();
+  //    }
+  //  }
+  //  return results;
+  //}
+  //
+  //public static void main(String[] args) {
+  //  File file = new File("C:\\Users\\DELL\\Desktop\\my");
+  //  File[] fs = file.listFiles();
+  //  for (File f : fs) {
+  //    reduceImg(f.getAbsolutePath(), 0.3f);
+  //  }
+  //}
 }
